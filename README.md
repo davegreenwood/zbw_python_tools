8d7d73b42ebf3283383a3a15c2daaa4bfef860c2
zeth willie <zethwillie@gmail.com>
2012-08-30 23:09:55 -0400

    tweaked readme, added a couple files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ zbw_python_tools
 ================
 
 just some stuff for maya
+
+
+TO DO: 8/30/12
+need to standardize function calls and all that. . . 
--- checkSysPaths.py
+++ checkSysPaths.py
@@ -5,7 +5,6 @@ syspaths = sys.path
 for path in syspaths:
 	print path
 
-
 # have to split these before printing out. make an interface
 # scriptPaths = mel.getenv("MAYA_SCRIPT_PATH")
 # plugInPaths = mel.getenv("MAYA_PLUG_IN_PATH")
