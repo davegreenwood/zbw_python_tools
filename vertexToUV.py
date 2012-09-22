acf216e4f26228db73065631a200fd7668ae8843
zeth willie <zethwillie@gmail.com>
2012-09-22 13:02:56 -0400

    working on it

--- vertexToUV.py
+++ vertexToUV.py
@@ -1,15 +1,19 @@
-#get up to mesh data from vertex selection at some point
+import maya.cmds as cmds
+
+#-------multiple selection and average uv position?
+#-------grab an edge and convert to 2 verts and get average. . .
 
 #get vertex
-
 vertex = cmds.ls(sl=True, fl=True)
-print vertex
 
+#convert vertex to uvs
+uvs = cmds.polyListComponentConversion(fv=True, tuv=True)
+#flatten the uv to list and grab first (cuz verts can be multiple uv's)
+uv = cmds.ls(uv, fl=True)[0]
 
-uv = cmds.polyListComponentConversion(fv=True, tuv=True)
-print uv
-
+#select our uv
 cmds.select(uv, r=True)
 #convert the uv index to u and v values
-uvVals = cmds.polyEditUV(q=True)
-print uvVals
+uvVal = cmds.polyEditUV(q=True)
+cmds.select(vertex)
+return(uvVal)
