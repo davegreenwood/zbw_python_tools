4785c9a48e4c5ee9fec37767bf560d6d5a7bc6e3
zeth willie <zethwillie@gmail.com>
2012-09-28 23:24:00 -0400

    comments on vertex to uv

--- vertexToUV.py
+++ vertexToUV.py
@@ -1,11 +1,15 @@
 import maya.cmds as cmds
 
-#-------multiple selection and average uv position?
+#-------could also select edit point????
+#-------multiple selection and average uv position? Orrrr option to create multiple UV's, one on each vertex
 #-------grab an edge and convert to 2 verts and get average. . .
+#-------have option for distributed (select 2 verts and number of follicles, spread that num between the two uv positions)
 
 #get vertex
 vertex = cmds.ls(sl=True, fl=True)
+#------check what type of selection you have and convert it to verts
 
+#-------for loop here. . . .
 #convert vertex to uvs
 uvs = cmds.polyListComponentConversion(fv=True, tuv=True)
 #flatten the uv to list and grab first (cuz verts can be multiple uv's)
