9411965e08deb9aad280147c959acaf194659ec7
zeth willie <zeth@catbuks.com>
2012-12-03 23:09:19 -0500

    slight changes

--- softModDeformer.py
+++ softModDeformer.py
@@ -1,8 +1,19 @@
+import maya.cmds as cmds
+
+
 # Here's how it goes . . .
 # choose a vert (for example)
-# get the uv position of that vert
-# create a soft mod there
+#TO-DO----------------check for just one vert . . .  
+vertex = cmds.ls(sl=True)[0]
+obj = vertex.partition(".")[0]
 
+vertPos = cmds.pointPosition(vertex)
+print(vertPos)
+
+cmds.select(obj)
+# create a soft mod there
+cmds.softMod(relative=False, falloffCenter = vertPos, falloffRadius=3.0)
+# get position of the softmod
 # create a control at the position of the softmod
 # connect the pos, rot, scale of the control to the softModHandle
 # create an attr on the control for the falloff (and the envelope)
