b87439f8634cdf495f7951170df1f8ac12fca7e3
zeth willie <zeth@catbuks.com>
2012-12-09 22:02:06 -0500

    closer to right

--- haveRibbonSetupCtrlJnts.py
+++ haveRibbonSetupCtrlJnts.py
@@ -1,25 +1,34 @@
 import maya.cmds as cmds
 import zbw_rig as rig
 
-#TO-DO----------------already have ribbon with shitton of joints. Deleted control structure. Create a chain for the master controls
+#already have ribbon with shitton of joints. Deleted control structure. Create a chain for the master controls
+def ctrlSetup():
 
-cmds.select(hi=True)
-sel = cmds.ls(sl=True)
-ctrlList = []
-groupList = []
+	cmds.select(hi=True)
+	sel = cmds.ls(sl=True)
+	ctrlList = []
+	groupList = []
 
-for x in sel:
-	if (cmds.listRelatives(x, p=True)):
-		cmds.parent(x, w=True)
-		#could rename joint here
-	ctrl = rig.createControl("%s_CTRL"%x, "cube", "x", "red")
-	ctrlList.append(ctrl)
-	group = rig.groupOrient(x, ctrl)
-	groupList.append(group)
-	cmds.parent(x, ctrl)
-	
-	
-for x in range(len(ctrlList)-1, 0, -1):
-	cmds.parent(groupList[x], ctrlList[x-1])
-	
-#TO-DO----------------for ribbon spine with controls, parent the group below to the control (joints are parented under the control). Then you can put a "local" control onto the joint to do the up/down stuff for each control
+	for x in sel:
+		if (cmds.listRelatives(x, p=True)):
+			cmds.parent(x, w=True)
+			#could rename joint here
+		ctrl = rig.createControl("%s_CTRL"%x, "cube", "x", "red")
+		ctrlList.append(ctrl)
+		group = rig.groupOrient(x, ctrl)
+		groupList.append(group)
+		cmds.parent(x, ctrl)
+		
+		
+	for x in range(len(ctrlList)-1, 0, -1):
+		#create a control for the move of the joint
+		#snap this to the joint 
+		#parent the joint to the control
+
+		#switch this to parenting the new control to the other control . . . 
+		cmds.parent(groupList[x], ctrlList[x-1])
+
+		#strip to rotateTranslate, then lock the translate of the old control
+		#strip to translate of the new control . . . (lock other channels other than up?)
+		
+	#TO-DO----------------put a "local" control onto the joint to do the up/down stuff for each control
