31f673da5ac23c1ecb67428a48867ff5f95c4244
zeth willie <zethwillie@gmail.com>
2012-12-21 23:41:40 -0500

    started this code for a dashboard type thingy

---
+++ zbw_rigWin.py
@@ -0,0 +1,82 @@
+#create a dockable toolbar that runs a bunch of my scripts (either calls the window or runs some function, or creates a new little window with some options)
+
+import zbw_window as win
+import zbw_rig as rig
+import maya.cmds as cmds
+import maya.OpenMaya as om
+
+#clean up zbw_follicle?
+
+#create control
+def createControl(*args):
+	pass
+
+#zbw_message
+def message(*args):
+	pass
+
+#group orient
+def groupOrient(*args):
+	pass
+
+#align to uv
+def alignToUV(*args):
+	pass
+
+#zbw_attributes (rewrite as pymel,python)
+def attributes(*args):
+	pass
+
+#softmod deformer
+def softModDeformer(*args):
+	pass
+
+#softselect deformer
+def softSelectDeformer(*args):
+	pass
+
+#measure distance
+def measure(*args):
+	pass
+
+#scale stretch ik? translate stretch ik?
+
+#fk chain
+def fkChain(*args):
+	pass
+
+#joints from list
+def jointChain(*args):
+	pass
+
+#comet rename
+def cometRename(*args):
+	pass
+
+#comet joint orient
+def cometJointOrient(*args):
+	pass
+
+#abSymMesh
+def abSymMesh(*args):
+	pass
+
+#cvInverter?
+
+#zbw_ribbon
+def zbw_ribbon(*args):
+	pass
+
+#zbw_charRigger??
+
+#append to sys path
+def appendToPath(*args):
+	pass
+
+#vertext to uv (print uv)
+def vertexToUV(*args):
+	pass
+
+#zbw_shapeScale
+def shapeScale(*args):
+	pass
