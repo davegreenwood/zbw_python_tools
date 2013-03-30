d400bd000a6fea92ce444d00f3b134a2917983d8
zeth willie <zeth@catbuks.com>
2013-03-30 14:37:17 -0400

    a couple more things I wrote at panda

---
+++ zbw_huddle.py
@@ -0,0 +1,53 @@
+#write script that allows you to pick a pivot and the scale things closer to that, BUT not acutally scaling, but scaling the distance from that point
+
+import maya.cmds as cmds
+import maya.OpenMaya as om
+
+widgets = {}
+
+def huddleUI(*args):
+    if cmds.window("hudWin", exists=True):
+        cmds.deleteUI("hudWin")
+        
+    widgets["win"] = cmds.window("hudWin", t="zbw_huddle", w=400, h=100)
+    
+    widgets["CLO"] = cmds.columnLayout()
+
+    widgets["slider"] = cmds.floatSliderGrp(min=0, max=2, f=True, label="Huddle Scale Factor:", cal=([1, "left"], [2,"left"], [3,"left"]), cw=([1,100],[2,75],[3,225]), pre=3, v=1.0)
+    cmds.separator(h=20)
+    widgets["button"] = cmds.button(l="Move objects aroudnd first selected", w=200, h=50, bgc=(.6,.8, .6), c=huddleExec)
+
+    cmds.showWindow(widgets["win"])
+    cmds.window(widgets["win"], e=True, w=200, h=100)
+    
+def huddleExec(*args):
+    factor = cmds.floatSliderGrp(widgets["slider"], q=True, v=True)
+    sel = cmds.ls(sl=True, type="transform")
+
+    center = sel[0]
+
+    objs = sel[1:]
+
+    centerPos = cmds.xform(center, q=True, ws=True, rp=True)
+    centerVec = om.MVector(centerPos[0], centerPos[1], centerPos[2])
+    # print (centerVec[0], centerVec[1], centerVec[2])
+    
+    for obj in objs:
+        # get location
+        objPos = cmds.xform(obj, ws=True, q=True, rp=True)
+        objVec = om.MVector(objPos[0], objPos[1], objPos[2])
+        # print (objVec[0], objVec[1], objVec[2])
+    
+        #find difference vector between obj and center
+        diffVec = objVec-centerVec
+        #scale that vector 
+        scaledVec = diffVec * factor
+        
+        #add that vector to the center vec
+        newVec = scaledVec + centerVec
+        
+        #apply it to the position of the obj
+        cmds.xform(obj, ws=True, t=(newVec[0], newVec[1], newVec[2]))
+        
+def huddle(*args):
+    huddleUI()
