343e07d42c25c660257eb1a9fcce265a8536afeb
zeth willie <zethwillie@gmail.com>
2013-05-02 16:50:23 -0400

    looking at this (maybe roll it up into the snap functionality or visa versa?)

--- copyTransforms.py
+++ copyTransforms.py
@@ -1,19 +1,19 @@
 #TO-DO----------------create UI. . . (this is already done, btw)
-#TO-DO----------------options for showing values captured, for copying only selected attr. .. for multiple keys? ? 
-
+#TO-DO----------------options for showing values captured, for copying only selected attr. .. for multiple keys? ?
+#TO-DO----------------copy and store? or copy from the object at that frame (other object?
 import maya.cmds as cmds
 
 sel = cmds.ls(sl=True, type="transform")
 if (len(sel)) > 1:
     base = sel[0]
-    
+
     objs = sel[1:]
 
     #get transforms
     pos = cmds.getAttr("%s.translate"%base)
     rot = cmds.getAttr("%s.rotate"%base)
     scale = cmds.getAttr("%s.scale"%base)
-    
+
     #put transforms
     for obj in objs:
         cmds.setAttr("%s.translate"%obj, pos[0][0], pos[0][1], pos[0][2])
