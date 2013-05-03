3a5218bf4052072033492a4dfb7533d4800e2f90
zeth willie <zethwillie@gmail.com>
2013-05-03 17:12:59 -0400

    not much

--- shitIWroteAtBuck.py
+++ shitIWroteAtBuck.py
@@ -10,7 +10,7 @@ cmds.sets(text="gCharacterSet", name="headControls")
 
 for obj in sel:
     cmds.sets("headControls", e=True, add=obj)
-	
+
 
 #apply a series of blend shapes to a series of objects
 
@@ -21,11 +21,11 @@ sel = cmds.ls(sl=True)
 for x in range(0, len(sel)):
     num = (x%10)+1
     thisTarget = "lf_stripWaveDriver%02d_geo"%num
-    
+
     BS = cmds.blendShape(thisTarget, sel[x], origin="local")
     cmds.blendShape(BS, e=True, w=[(0,1)])
-	
-	
+
+
 #to create bend deformers on each of the driving cards and orient them correctly
 
 import maya.cmds as cmds
@@ -39,8 +39,8 @@ for obj in sel:
 
     cmds.xform(bendHandle, os=True, r=True, ro=(0,0,0))
     cmds.xform(bendHandle, os=True, r=True, t=(0,1,0))
-	
-	
+
+
 #to replace side card geo with relatively shifted versions of the first one selected (so that can drive blend shapes)
 
 import maya.cmds as cmds
@@ -54,14 +54,14 @@ base = sel[0]
 #loop through the rest, get the pos of the card, dupe the bsae, move it to the pos and delete the orig card
 for x in range(1, len(sel)):
     cardPos = cmds.xform(sel[x], q=True, ws=True, rp=True)
-    
+
     newCard = cmds.duplicate(base, n="%s_1"%sel[x])
- 
+
     cmds.xform(newCard, ws=True, t=cardPos)
-    
+
     cmds.delete(sel[x])
 
-	
+
 #code to reorder the deformers so the scales happen (tweak, lattice)
 
 sel = cmds.ls(sl=True)
@@ -71,7 +71,8 @@ baseAttr = "COG_CTRL.scalesPuffer"
 for obj in sel:
     #get tweak deformer on obj
     shape = cmds.listRelatives(obj, s=True)
-    tweak = cmds.listConnections(shape)[8]
+    tweak = cmds.listConnections(shape)[8] #here should use tweak = cmds.listConnections(shape, type="tweak")
+
     #print(tweak)
     #reorder two deformers
     cmds.reorderDeformers(tweak, "ffd1", obj)
@@ -86,13 +87,13 @@ for obj in objs:
     parent = cmds.listRelatives(obj, p=True)[0]
     rot = cmds.xform(obj, q=True, ws=True, ro=True)
     pos =cmds.xform(obj, q=True, ws=True, rp=True)
-    
+
     grp = cmds.group(empty=True, n=(obj+"_GRP"))
     cmds.xform(grp, ws=True, ro=rot)
     cmds.xform(grp, ws=True, t=pos)
     cmds.parent(obj, grp)
     cmds.parent(grp, parent)
-	
+
 #to change the local rotational axis
 
 import maya.cmds as cmds
@@ -100,8 +101,8 @@ import maya.cmds as cmds
 sel = cmds.ls(sl=True)
 for obj in sel:
     cmds.xform(obj, os=True, r=False, ra=(0, 180, 23.5))
-	
-	
+
+
 #to put select objs' pivots onto the closest point on a mesh that sort of tracks where you want them to be
 
 import maya.cmds as cmds
@@ -115,13 +116,13 @@ for obj in objs:
     #get position
     objPos = cmds.xform(obj, q=True, ws=True, rp=True)
     cmds.setAttr("%s.inPosition"%cpom, objPos[0], objPos[1], objPos[2])
-    
+
     #get position on surface
     pos = cmds.getAttr("%s.position"%cpom)
-    
+
     #change pivot
     cmds.xform(obj, ws=True, piv=pos[0])
-	
+
 #to connect a vis attr to the vis of the shape nodes
 
 import maya.cmds as cmds
@@ -132,7 +133,7 @@ for ctrl in sel:
     thisShape = cmds.listRelatives(ctrl, s=True, c=True)
     cmds.connectAttr("COG_CTRL.fineShaperCtrls", "%s.visibility"%thisShape[0])
 
-#to add controls to selected cvs (via cluster, replace weighted node w/control)	
+#to add controls to selected cvs (via cluster, replace weighted node w/control)
 import maya.cmds as cmds
 import zbw_rig as rig
 
@@ -148,7 +149,7 @@ for i in range(len(sel)):
     #replace the weighted node of the cluster with the circle
     clusHandShape = cmds.listRelatives(cluster[1], s=True)
     cmds.cluster(cluster[0], e=True, bs=1, wn=(ctrl, ctrl))
-    
+
     cmds.setAttr("%s.originX"%clusHandShape[0], 0.0)
     cmds.setAttr("%s.originY"%clusHandShape[0], 0.0)
     cmds.setAttr("%s.originZ"%clusHandShape[0], 0.0)
